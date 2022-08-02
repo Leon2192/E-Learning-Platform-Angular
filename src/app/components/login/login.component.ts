b24a0850598fcb7f210534294c6d6e1fbba0c27a
Leonardo <leon.strokes@gmail.com>
2022-08-02 20:10:30 -0300

    Forms

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
+
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
+    this.form = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  ingresar() {
+    const usuario = this.form.value.usuario;
+    const password = this.form.value.password;
+
+    if (usuario == 'leonardo' && password == 'arrieta') {
+      //this.loading = true;
+      this.load();
+    } else {
+      // Pasa otra cosa
+      this.error();
+      this.form.reset();
+    }
+  }
+
+  load() {
+    this.loading = true;
+    setTimeout(() => {
+      // Redirect
+      this.loading = false;
+    }, 5000);
+  }
+
+  error() {
+    this._snackBar.open('Usuario o contraseña son inválidos', '', {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  }
+}
